9c81d26804fbf03ad4b10b9619b1acccb96362ca
RonakGamer <ronakmusic31@gmail.com>
2020-09-28 18:35:37 +0530

    Updates

---
+++ precache-manifest.e7e7aa317887cfa2c791d6e51ebabc11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d040166208f24437b01ff12dfc2c03",
+    "url": "/React-Project/index.html"
+  },
+  {
+    "revision": "f89227e29735d287717c",
+    "url": "/React-Project/static/css/main.761a99f7.chunk.css"
+  },
+  {
+    "revision": "b50ad5dfd630ed1c1f97",
+    "url": "/React-Project/static/js/2.676dfd6a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-Project/static/js/2.676dfd6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f89227e29735d287717c",
+    "url": "/React-Project/static/js/main.fba00b46.chunk.js"
+  },
+  {
+    "revision": "20a60f1123c000070fe5",
+    "url": "/React-Project/static/js/runtime-main.ad0b95a0.js"
+  }
+]);
